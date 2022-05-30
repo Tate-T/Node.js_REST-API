6cce50ab08a1df16fb3b6f924406a089713a12c1
Tate-T <tatjana.tarasovych@gmail.com>
2022-05-30 19:29:30 +0300

    addContact req

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,14 +5,14 @@ const router = express.Router();
 const {
   listContacts,
   getContactById,
-  // addContact,
+  addContact,
   // removeContact,
   // updateContact,
 } = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
   const contacts = await listContacts();
-  res.status(200).json(contacts)
+  return res.status(200).json(contacts)
 })
 
 router.get('/:contactId', async (req, res, next) => {
@@ -25,7 +25,12 @@ router.get('/:contactId', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    return res.status(201).json(await addContact(req.body))
+  } catch {
+    return res.status(400).json({ message: 'missing required name field' })
+  }
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
