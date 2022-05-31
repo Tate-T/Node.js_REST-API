e0d699bdd98852b99250e79a1ac186dac4a1939f
Tate-T <tatjana.tarasovych@gmail.com>
2022-05-31 21:37:15 +0300

    MongoClient

--- models/mongodb.js
+++ models/mongodb.js
@@ -6,24 +6,18 @@ const path = require('path');
 dotenv.config({ path: path.join(__dirname. '.env') })
 
 const url = process.env.MONGODB_URL;
-const client = new MongoClient(url);
-
-// Database Name
 const dbName = process.env.MONGODB_NAME;
 
 async function main() {
-    // Use connect method to connect to the server
+    const client = new MongoClient(url);
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
-    const collection = db.collection('documents');
+    const dbContacts = db.collection('documents');
 
-    // the following code examples can be pasted here...
+    dbContacts.insertOne(contacts);
 
     return 'done.';
 }
 
-main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
+main();
