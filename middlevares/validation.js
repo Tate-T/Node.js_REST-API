e5fa1f9bacc8c5833b3a6f0cc26307aec1c00bec
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-01 16:43:55 +0300

    fixed routes

--- middlevares/validation.js
+++ middlevares/validation.js
@@ -25,5 +25,16 @@ module.exports = {
 
         next();
     },
+    updateEmailValidation: (req, res, next) => {
+        const schema = Joi.object({
+            email: Joi.string().email().required(),
+        });
+
+        const validationResult = schema.validate(req.body);
+        if (validationResult.error)
+            return res.status(400).json({ message: validationResult.error.details });
+
+        next();
+    }
 }
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,16 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
-const { userValidation } = require('../../middlevares/validation');
+const { userValidation, updateEmailValidation } = require('../../middlevares/validation');
 
 const { authCheck } = require('../../middlevares/authCheck');
+const multer = require('multer');
+const Jimp = require("jimp");
+const fs = require("fs").promises;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const { User } = require('../../db/usersAuth');
 const {
     registration,
     login,
     logout,
-    current
+    current,
+    updateEmail
 } = require('../../models/auth');
 
 router.post('/register', userValidation, async (req, res, next) => {
@@ -47,5 +53,46 @@ router.get("/current", authCheck, async (req, res, next) => {
         return res.status(401).json({ message: 'Not authorized' });
     res.status(200).json({ user: result });
 });
+
+router.patch("/avatars", multer.single("avatar"), async (req, res, next) => {
+    const { description } = req.body;
+    const { path: temporaryName, originalname } = req.file;
+    const newPathName = path.join(
+        process.cwd(),
+        `public/avatars/${uuidv4()}_${originalname}`
+    );
+
+    Jimp.read(temporaryName, async (err, storeImage) => {
+        if (err) throw err;
+        storeImage
+            .resize(250, 250) // resize
+            .quality(60) // set JPEG quality
+            .write(newPathName); // save
+        await fs.unlink(temporaryName);
+    });
+
+    res.json({ description, message: "Файл успешно загружен", status: 200 });
+});
+
+router.get("/verify/:verificationToken", async (req, res, next) => {
+    const { verificationToken } = req.params;
+    const result = await registration(verificationToken);
+    if (!result) {
+        return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json({ message: "Verification successful" });
+});
+
+router.post(
+    "/verify",
+    updateEmailValidation,
+    async (req, res, next) => {
+        const { email } = req.body;
+        const result = await updateEmail(email);
+        if (typeof result === "string")
+            return res.status(400).json({ message: result });
+        if (result) res.json({ message: "Verification email sent" });
+    }
+);
 
 module.exports = router;
