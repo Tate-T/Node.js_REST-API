6c9d9838887d31b7977f357d51231bd526734a1c
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-01 14:03:35 +0300

    auth points

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./routes/api/contacts');
+const usersRouter = require('./routes/api/auth');
 
-const app = express()
+const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -13,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/users', usersRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,48 @@ const router = express.Router();
 
 const { validation } = require('../../middlevares/validation');
 
+const { authCheck } = require('../../middlevares/authCheck');
+
+const { User } = require('../../models/auth');
 const {
     registration,
     login,
+    logout,
+    current
 } = require('../../models/auth');
+
+router.post('/register', validation, async (req, res, next) => {
+    const userMail = req.body.email
+    const existingUser = await await User.findOne({ userMail });
+
+    if (existingUser) {
+        return res.status(409).json({ message: "Email in use" });
+    }
+
+    const user = await registration(req.body);
+
+    res.status(201).json(user);
+});
+
+router.post("/login", validation, async (req, res, next) => {
+    const result = await login(req.body);
+    if (!result)
+        return res.status(401).json({ message: 'Email or password is wrong' });
+    res.status(200).json({ user: result });
+});
+
+router.get("/logout", authCheck, async (req, res, next) => {
+    const result = await logout(req.id);
+    if (!result)
+        return res.status(401).json({ message: 'Not authorized' });
+    res.status(204).json({ message: "No Content" });
+});
+
+router.get("/current", authCheck, async (req, res, next) => {
+    const result = await current(req.id);
+    if (!result)
+        return res.status(401).json({ message: 'Not authorized' });
+    res.status(200).json({ user: result });
+});
+
+module.exports = router;
