b8ecdb0216970596c0e5c048a775cc38e6d65565
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-01 16:24:51 +0300

    verificationToken

--- db/usersAuth.js
+++ db/usersAuth.js
@@ -19,6 +19,14 @@ const contactsSchema = new mongoose.Schema({
         type: String,
         default: null
     },
+    verify: {
+        type: Boolean,
+        default: false,
+    },
+    verificationToken: {
+        type: String,
+        required: [true, 'Verify token is required'],
+    },
 });
 
 const User = mongoose.model("User", contactsSchema);
--- models/auth.js
+++ models/auth.js
@@ -1,14 +1,27 @@
 const { User } = require('../db/usersAuth');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const sgMail = require("@sendgrid/mail");
 
 const registration = async ({ email, password }) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     try {
         const user = new User({
             email,
             password: await bcrypt.hash(password, 10)
         })
         await user.save();
+
+        const msg = {
+            to: email,
+            from: "tatjana.tarasovych@gmail.com",
+            subject: "Please, confirm Your Email!",
+            text: `Here is Your verification link - http://127.0.0.1:3000/users/verify/${user.verificationToken}`,
+            html: `Here is Your verification <a href=http://127.0.0.1:3000/users/verify/${user.verificationToken}>link</a>`,
+        };
+
+        await sgMail.send(msg);
+
         const newUser = { email: user.email, subscription: user.subscription }
         return newUser
 
@@ -57,6 +70,27 @@ const current = async (email) => {
     }
 }
 
+const updateEmail = async (email) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return `No user with email ${email}`;
+        if (user.verify === true) return "Verification has already been passed";
+        const msg = {
+            to: email,
+            from: "tatjana.tarasovych@gmail.com",
+            subject: "Please, confirm Your Email!",
+            text: `Here is Your verification link - http://127.0.0.1:3000/users/verify/${user.verificationToken}`,
+            html: `Here is Your verification <a href=http://127.0.0.1:3000/users/verify/${user.verificationToken}>link</a>`,
+        };
+
+        await sgMail.send(msg);
+        return user;
+    } catch (error) {
+        console.log("error", error.message);
+        return false;
+    }
+}
 module.exports = {
-    registration, login, logout, current
+    registration, login, logout, current, updateEmail
 }
